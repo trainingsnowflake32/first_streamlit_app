15bcff8db215c238be163344284aae4bfca95fba
training snowflake <115386828+trainingsnowflake32@users.noreply.github.com>
2022-10-09 00:36:58 -0400

    Update streamlit_app.py
    updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.title('Menu')
+
+streamlit.title('breakfast')
+streamlit.title('lunch')
+streamlit.title('dinner')
+streamlit.title('drinks')
